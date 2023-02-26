3ee9c9b0a47c69d4d6a3ebb734205172ad8e5ae5
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2023-02-25 23:26:27 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+class Socket
+{
+  ws = null;
+  // Helper with evt2async
+  #requestCanary = 0;
+  #msgHandlerOn = false;
+  #currentData = new Uint8Array(0);
+  
+  sendAndWait(data)
+  {
+    if(data.constructor !== Uint8Array) throw new TypeError('data must be a Uint8Array');
+    let oldRequestCanary = this.#requestCanary;
+    if(!this.#msgHandlerOn)
+    {
+      this.ws.addEventListener("message", this.onMessage.bind(this));
+      this.#msgHandlerOn = true;
+    };
+    let hdr = new Uint8Array(6);
+    (new Uint16Array(hdr.buffer))[0] = 33253; // PNP in base36
+    (new Uint32Array(hdr.buffer, 3))[0] = data.byteLength;
+    let newData = new Uint8Array(6 + data.byteLength);
+    newData.set(hdr);
+    newData.set(data, 6);
+    this.ws.send(newData);
+    while(oldRequestCanary !== this.requestCanary)
+      await new Promise((resolve, ..._) => setTimeout(resolve, 50));
+    return this.#currentData;
+  };
+  
+  onMessage(ev)
+  {
+    this.#currentData = ev.data;
+    this.#requestCanary++;
+  };
+};
